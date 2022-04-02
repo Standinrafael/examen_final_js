1f4c9edcc53a59e8270a625de40ded620436914f
Jorge Sanchez <jlsanchez1997@hotmail.es>
2022-04-01 21:06:27 -0500

    tecer commit

--- src/main/java/ec/edu/uce/controller/VueloController.java
+++ src/main/java/ec/edu/uce/controller/VueloController.java
@@ -1,5 +1,6 @@
 package ec.edu.uce.controller;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import ec.edu.uce.modelo.CompraPasaje;
 import ec.edu.uce.modelo.Vuelo;
 import ec.edu.uce.service.IGestorClienteService;
 import ec.edu.uce.service.IGestorEmpleadoService;
+import ec.edu.uce.service.IVueloService;
 
 @Controller
 @RequestMapping("/vuelos/")
@@ -25,21 +28,53 @@ public class VueloController {
 	@Autowired
 	private IGestorEmpleadoService empleadoService;
 	
+	@Autowired
+	private IVueloService vueloService;
+	
 	
 	
 	//BuscarVuelosDisponibles
-	@GetMapping("buscar/{idEstudiante}")
-	public String obtenerUsuario(@PathVariable("idEstudiante") Integer idEstudiante, Model modelo) {
+	@GetMapping("buscar/{origen}/{destino}/{fecha}")
+	public String obtenerUsuario(@PathVariable("origen") String origen,String destino, LocalDateTime fecha, Model modelo) {
 
-		List<Vuelo> vuelos=this.clienteService.buscarVuelosDisponibles(null, null, null);
+		List<Vuelo> vuelos=this.clienteService.buscarVuelosDisponibles(origen, destino, fecha);
 
 
 		modelo.addAttribute("vuelos", vuelos);
 		return "lista";
 	}
 	
+	//Reservar Pasajes aereos
+	
+	@GetMapping("vueloActualiza/{numeroVuelo}")
+	public String obtenerReserva(@PathVariable("numeroVuelo") String numeroVuelo, Vuelo vuelo,
+			Model modelo) {
+		Vuelo vuelo1=this.clienteService.reservarPasajes(numeroVuelo);
+		modelo.addAttribute("vuelo", vuelo1);
+		
+		
+		return "vueloActualiza";
+	}
+	
+	@PutMapping("actualizarVuelo/{numeroVuelo}")
+	public String actualizarCita(@PathVariable("numeroVuelo") String numeroVuelo, Vuelo vuelo) {
+
+		vuelo.setNumero(numeroVuelo);
+		
+		if(vuelo.getEstado()=="Disponible") {
+			
+			
+		}else {
+			
+			vuelo.setEstado("No Disponible");
+			this.vueloService.actualizarVuelo(vuelo);
+		}
+
+		return "lista";
+	}
+	
 	//Realizar Check In
-	@GetMapping("citaActualiza/{numeroPasaje}")
+	@GetMapping("pasajeActualiza/{numeroPasaje}")
 	public String obtenerPaginaCheckIn(@PathVariable("numeroPasaje") String numerPasaje,CompraPasaje compraPasaje ,
 			Model modelo) {
 		
